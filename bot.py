7303aed8256519dcc992dd36db90e2941704a123
APIPanakdewa <152812646+APIPanakdewa@users.noreply.github.com>
2026-06-25 18:25:59 +0700

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,24 @@
+import os
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv("TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+intents.voice_states = True
+
+bot = commands.Bot(
+    command_prefix="!",
+    intents=intents
+)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+
+bot.run(TOKEN)
